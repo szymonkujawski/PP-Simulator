1449ef6606a5873d4d1cfc046520f744510fc4da
szymonkujawski <231327@student.uek.krakow.pl>
2024-10-29 22:30:06 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,7 +1,30 @@
 ﻿namespace Simulator;
 public class Animals
 {
-    public required string Description { get; init; }
+    private string description = "Unknown";
+    public required string Description
+    {
+        get => description;
+        init
+        {
+            value = value.Trim();
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }else if (value.Length > 15)
+            {
+                value = value.Substring(0, 15).TrimEnd();
+                if (value.Length < 3)
+                {
+                    value = value.PadRight(3, '#');
+                }
+            }if (char.IsLower(value[0]))
+            {
+                value = char.ToUpper(value[0]) + value.Substring(1);
+            }
+            description = value;
+        }
+    }
 
     public uint Size { get; set; } = 3;
 
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,25 +1,56 @@
 ﻿namespace Simulator;
+
 internal class Creature
 {
-    private int level;
+    private string name = "Unknown";
+    public string Name
+    {
+        get => name;
+        init
+        {
+            value = value.Trim();
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }else if (value.Length > 25)
+            {
+                value = value.Substring(0, 25).TrimEnd();
+                if (value.Length < 3)
+                {
+                    value = value.PadRight(3, '#');
+                }
+            }if (char.IsLower(value[0]))
+            {
+                value = char.ToUpper(value[0]) + value.Substring(1);
+            }
 
-    public int Level { get; set; }
-
-    private string? name;
-
-    public string? Name { get; set; }
-
+            name = value;
+        }
+    }
+    private int level = 1;
+    public int Level
+    {
+        get => level;
+        init
+        {
+            level = value < 1 ? 1 : (value > 10 ? 10 : value);
+        }
+    }
 
     public string Info => $"{Name} - [{Level}]";
 
     public Creature(string name, int level = 1)
     {
-
         Name = name;
         Level = level;
-
     }
+
     public Creature() { }
 
     public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+
+    public void Upgrade()
+    {
+        if (level < 10){level++;}
+    }
 }
